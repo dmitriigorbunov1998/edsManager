a9d955f5d9747a180da4b3e0db7a03df7d765ce8
Dmitrii Gorbunov <dmitriigorbunov1998@gmail.com>
2026-06-29 15:02:52 +0300

    (EDS): feature/added-loading-spinner-for-AppComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { Dashboard } from './pages/Dashboard/Dashboard.tsx';
 import './App.css';
+import { Suspense } from 'react';
+
+function LoadingSpinner() {
+    return (
+        <div className="app-loading">
+            <div className="app-loading-spinner" />
+            <p className="app-loading-text">Загрузка...</p>
+        </div>
+    );
+}
 
 function App() {
   return (
-    <Dashboard />
+    <Suspense fallback={<LoadingSpinner />}>
+      <Dashboard />
+    </Suspense>
   );
 }
 
