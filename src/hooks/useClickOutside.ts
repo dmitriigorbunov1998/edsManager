d790c0c4477ad96f4bf019e834c9a1b13848f919
Dmitrii Gorbunov <dmitriigorbunov1998@gmail.com>
2026-07-13 01:11:52 +0300

    (UDS): chore/prettier-fixed-hooks

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -2,17 +2,17 @@ import { useEffect } from 'react';
 import type { RefObject } from 'react';
 
 export function useClickOutside<T extends HTMLElement>(
-    ref: RefObject<T | null>,
-    handler: () => void
+  ref: RefObject<T | null>,
+  handler: () => void
 ) {
-    useEffect(() => {
-        const handleClickOutside = (e: MouseEvent) => {
-            if (ref.current && !ref.current.contains(e.target as Node)) {
-                handler();
-            }
-        };
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        handler();
+      }
+    };
 
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => document.removeEventListener('mousedown', handleClickOutside);
-    }, [ref, handler]);
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [ref, handler]);
 }
